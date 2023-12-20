6f961922c04fb990cedfc94410bc7bb917d90c05
Knight <153969695+kn-ght@users.noreply.github.com>
2023-12-20 12:23:15 -0500

    Update ActiveDirToFormatedXML.py

--- LDAP/ActiveDirToFormatedXML.py
+++ LDAP/ActiveDirToFormatedXML.py
@@ -2,44 +2,55 @@ serial = input("serial address for device")
 from ldap3 import Server, Connection, ALL, SAFE_SYNC
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
-server = Server('ldap://172.17.101.162:389')
-conn = Connection(server, 'CN=Administrator, CN=Users,DC=knight,DC=local', 'BOCES10!', client_strategy=SAFE_SYNC, auto_bind=True)
-status, result, response, _ = conn.search('OU=KnightCorp,DC=knight,DC=local', '(objectclass=organizationalPerson)', attributes='*')
+#change based on your server
+server = Server('ldap://serverip:389')
+#change based upon your server
+conn = Connection(server, 'CN=adminlogin, CN=---,DC=---,DC=local', 'password', client_strategy=SAFE_SYNC, auto_bind=True)
+status, result, response, _ = conn.search('OU=organizationalunit,DC=---,DC=local', '(objectclass=organizationalPerson)', attributes='*')
 root = ET.Element("directory")
 item_list = ET.SubElement(root, "item_list")
 list_of_dicts = [dict(obj) for obj in response]
-
 infodicts = []
-key = "raw_attributes"
-current = {}
+favnum = 0
+scraped = []
+for i,dics in enumerate(list_of_dicts):
+    #print(dics['raw_attributes'],'\n')
+    infodicts.append(dics['raw_attributes'])
 
-# Display the result
-for i, obj_dict in enumerate(list_of_dicts):
-    #print(f"Dictionary {i + 1}: {obj_dict} \n")
-    current = obj_dict[key]
-        #print(current["givenName"],"\n")
+for i in infodicts:
+    las = i['sn']
+    las=str(las)
+    last = las[3:-2]
+    firs=i['givenName']
+    firs=str(firs)
+    first = firs[3:-2]
+    phon=i['telephoneNumber']
+    phon=str(phon)
+    phone=phon[3:-2]
+    now = {'first':first,'last':last,'phone':phone}
+    scraped.append(now)
+scraped.sort(key=lambda x: x['last'])
+
+for i in scraped:
     item = ET.SubElement(item_list, "item")
     ln = ET.SubElement(item, "ln")
-    sn= current['sn']
-    text_string = str(sn)
-    result = text_string[2:-1]
+    sn= i['last']
+    result= str(sn)
     ln.text = result
 
     fn = ET.SubElement(item, "fn")
-    first = current['givenName']
-    text_string = str(first)
-    result = text_string[2:-1]
+    first = i['first']
+    result = str(first)
     fn.text = result
 
     ct = ET.SubElement(item, "ct")
-    num = current['telephoneNumber']
-    text_string =str(num)
-    result = text_string[2:-1]
+    num = i['phone']
+    result = str(num)
     ct.text = result
 
     sd = ET.SubElement(item, "sd")
-    sd.text = str(i)
-
+    sd.text = str(favnum)
+    favnum+=1
     rt = ET.SubElement(item, "rt")
     rt.text = "3"
 
@@ -57,12 +68,9 @@ for i, obj_dict in enumerate(list_of_dicts):
 
     bb = ET.SubElement(item, "bb")
     bb.text = "0"
-
-tree = ET.ElementTree(root)
-xml_str = ET.tostring(root, encoding='utf-8').decode()
-xml_str = minidom.parseString(xml_str).toprettyxml(indent="    ")
-xml_str = f'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n<!-- $RCSfile$  $Revision: 35928 $ -->\n{xml_str}'
-
-# Write to the XML file
 with open(serial+"-directory.xml", "w") as file:
+    tree = ET.ElementTree(root)
+    xml_str = ET.tostring(root, encoding='utf-8',xml_declaration=True).decode()
+    xml_str = minidom.parseString(xml_str).toprettyxml(indent="    ")
+    xml_str = f'{xml_str}'
     file.write(xml_str)
